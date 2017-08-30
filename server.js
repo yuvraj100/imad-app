614d286021380be636da7f91ea34e778f8c36d68
YUVRAJ KUMAR <yuvraj.cusat@gmail.com>
2017-08-31 00:40:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,19 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one',function(req , res){
+    res.send('article one is requsted by client');
+});
+
+app.get('/article-two',function(req , res){
+    res.send('article two is requsted by client');
+});
+
+app.get('/article-three',function(req , res){
+    res.send('article three is requsted by client');
+});
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
