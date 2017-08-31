961abaf25d813616d198bdf0fcef91f56515faa7
YUVRAJ KUMAR <yuvraj.cusat@gmail.com>
2017-08-31 20:05:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req , res){
-    res.sendFile(path.join(__dirname,'ui', 'the C concepts.html'));
+    res.sendFile(path.join(__dirname,'ui', 'the-C-concepts.html'));
 });
 
 
