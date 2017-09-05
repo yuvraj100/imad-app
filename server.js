66cb9caecbbdf4bcbcbd8e64b883d1ec61a3d2ab
YUVRAJ KUMAR <yuvraj.cusat@gmail.com>
2017-09-05 13:58:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,61 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = {
+    title: 'The C concepts | yuvraj',
+    heading : 'The C Concepts',
+    date : '5 sep 2017',
+    content : ` <p>
+                Lets begin with a quick introduction to C. Our aim is to learn the concept of C deeply and implement that concepts and logics in real program.
+                C is a general-purpose language. It has been closely associated with the UNIX system where it was developed, since both Sytem and most of the programs that run on it are written in C-lanugaue. The language, however, is not tied to any one operating system or machine; and although it has been called a "System Programming Language" because it is useful to writing compilers and Opearting systems, it has been used equally well to write major programs in many different domains.
+            </p>
+            <p>
+                C provides the fundamental control flow constructions required for well-structured programs: statement grouping, decision making(if-else), selecting one of a set of possible cases (switch), looping with termination test at the top(while, for) or at the bottom (do),  and early loop exit (break).
+            </p>`
+};
+function createTemplate (Data){
+var title = Data.title;
+var date = Data.date;
+var heading = Data.heading;
+var content = Data.content;
+var htmlTemplate = `
+<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <meta name = "viewport" content="width=device-width,intial-scale=1"/>
+      <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+        <div>
+            <a href = "/">Home</a>
+        </div>
+        <hr/>
+        <h3>${heading}</h3>
+        <h4>yuvraj</h4><br/>
+        
+        <div>
+            ${date}
+        </div>
+        
+        <div>
+           ${content}
+        </div>
+        </div>
+    </body>
+</html>
+`;
+return htmltemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one',function(req , res){
-    res.sendFile(path.join(__dirname,'ui', 'the-C-concepts.html'));
+    res.send(createTemplate(articleOne));
 });
 
 
