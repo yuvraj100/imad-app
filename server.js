a9b4d4cbb67f5a2a81dcc2d10bb6bf310c337e4f
YUVRAJ KUMAR <yuvraj.cusat@gmail.com>
2017-09-19 17:40:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -79,6 +79,11 @@ return htmlTemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+var counter = 0;
+app.get('/counter',function (req,res){
+   counter = counter +1;
+   res.send(counter.toString());
+});
 
 app.get('/:articlename',function(req , res){
     
