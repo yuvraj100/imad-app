41e7ccdf90bd4bb1c251860166d9a443f3956274
YUVRAJ KUMAR <yuvraj.cusat@gmail.com>
2017-09-19 19:00:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ var button = document.getElementById('counter');
 var counter = 0;
 button.onClick = function() {
     
-  counter = counters + 1;
+  counter = counter + 1;
   var span = document.getElementById('count');
   span.innerHTML = counter.toString();
 };
