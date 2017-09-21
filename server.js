b8f089937b3180b5fd37972ada4faf36c801d5a0
YUVRAJ KUMAR <yuvraj.cusat@gmail.com>
2017-09-21 12:15:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ app.get('/counter',function (req,res){
 });
 
 var names = [];
-app.get('/submit-name/:name',function (req,res){
+app.get('/submit-name',function (req,res){
     
     var name = req.query.name;
     names.push(name);
