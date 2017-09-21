8b32c8010a5d7a4a1514831fc235f46db3aa61eb
YUVRAJ KUMAR <yuvraj.cusat@gmail.com>
2017-09-21 11:53:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,38 @@
 var button = document.getElementById('counter');
 
+var submit = document.getElementById('submit_btn');
+submit.onclick = function ()
+{
+  var requestName = new XMLHttpRequest();
+  //creating request
+  
+  //request response code
+  requestName.onreadystatechange = function (){
+    
+    if(requestName.readyState == XMLHttpRequest.DONE)
+    {
+        if(requestName.status == 200){
+            
+            var names = requestName.responseText;
+            names = JSON.parse(names);
+            var list = '';
+            for(var i =0;i<names.length ;i++)
+            {
+                list = '<li>'+names[i]+'</li>';
+            }
+            var ul = document.getElementById('nameList');
+            ul.innerHTML = list;
+        }
+    }
+  };
+  var nameInput = document.getElementById('name');
+  var name = nameInput.value;
+  requestName.open('GET','http://yuvrajcusat.imad.hasura-app.io/submit-name?name='+name,true);
+  requestName.send(null);
+};
+
+
+
 button.onclick = function () {
     
     var request = new XMLHttpRequest();
